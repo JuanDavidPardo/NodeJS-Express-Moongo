bd7fce23a5390177bbd296846ca6cd764c114bf4
juandavidpardoreina <juan23.pa@gmail.com>
2018-02-01 23:05:37 -0500

    Connection to moono, encrypt password and flash messages

---
+++ config/passport.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require('passport-local').Strategy;
+const moongose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// local user model 
+const User = moongose.model('users');
+
+module.exports = function(passport){
+    passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
+        User.findOne(
+            {email : email}
+        ).then( user => {
+            // Matched use
+            if(!user){
+                return done(null, false, {message: 'User not found'});
+            }
+
+            // Match password
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err)throw err;
+                if(isMatch){
+                    return done(null, user);
+                }else{
+                    return done(null, false, {message: 'Password incorrect'});
+                }
+            });
+        });
+    }));
+}
